8b763cf0300108e26508efb716f9f2b43e05ef43
Kyle King <KyleKing@users.noreply.github.com>
2024-10-05 19:35:22 -0400

    refactor: make configurable

--- my-index.ts
+++ my-index.ts
@@ -4,185 +4,194 @@ import {
   writeToProfile,
   mapSimultaneous,
   toKey,
-  toStickyModifier,
-  toNone,
 } from "karabiner.ts";
+
+const L_GUI = "a";
+const L_ALT = "s";
+const L_CTRL = "d";
+const L_SHIFT = "f";
+
+const R_SHIFT = "j";
+const R_ALT = "k";
+const R_CTRL = "l";
+const R_GUI = ";";
 
 writeToProfile(
   "Default profile",
   [
-    rule("Caps + Quote -> Hyper").manipulators([
-      map("⇪")
-        .toIfAlone("⇪", {}, { halt: true })
-        .toDelayedAction(toNone(), [
-          toStickyModifier("left_shift", "toggle"),
-          toStickyModifier("left_control", "toggle"),
-          toStickyModifier("left_option", "toggle"),
-          toStickyModifier("left_command", "toggle"),
-        ])
-        .toIfHeldDown("l⇧", "l⌘⌥⌃", { halt: true }),
-      map("'")
-        .toIfAlone("'", {}, { halt: true })
-        .toDelayedAction(toKey("vk_none"), toKey("'"))
-        .toIfHeldDown("r⇧", "r⌘⌥⌃", { halt: true })
-        .parameters({ "basic.to_if_held_down_threshold_milliseconds": 220 }),
-    ]),
+    // Alt: https://karabiner.ts.evanliu.dev/examples/modifier-keys/caps_lock-to-hyper
+    //rule("Caps + Quote -> Hyper").manipulators([
+    //  map("⇪")
+    //    .toIfAlone("⇪", {}, { halt: true })
+    //    .toDelayedAction(toNone(), [
+    //      toStickyModifier("left_shift", "toggle"),
+    //      toStickyModifier("left_control", "toggle"),
+    //      toStickyModifier("left_option", "toggle"),
+    //      toStickyModifier("left_command", "toggle"),
+    //    ])
+    //    .toIfHeldDown("left⇧", "left⌘⌥⌃", { halt: true }),
+    //  map("'")
+    //    .toIfAlone("'", {}, { halt: true })
+    //    .toDelayedAction(toKey("vk_none"), toKey("'"))
+    //    .toIfHeldDown("right⇧", "right⌘⌥⌃", { halt: true })
+    //    .parameters({ "basic.to_if_held_down_threshold_milliseconds": 220 }),
+    //]),
 
     // Home row mods
-    rule("Home row mods - shift, ctrl, opt, cmd").manipulators([
+    rule(`Home Row Mods (GUI: ${L_GUI}, Ctrl: ${L_CTRL}, Alt: ${L_ALT}, Shift: ${L_SHIFT})`).manipulators([
       //
       // Four - left hand
-      mapSimultaneous(["a", "s", "d", "f"]).toIfHeldDown("l⇧", ["l⌘⌥⌃"]),
+      mapSimultaneous([L_SHIFT, L_CTRL, L_ALT, L_GUI]).toIfHeldDown("left⇧", ["left⌘⌥⌃"]),
       //
       // Three - left hand
-      mapSimultaneous(["a", "s", "d"]).toIfHeldDown("l⇧", ["l⌥⌃"]),
-      mapSimultaneous(["a", "d", "f"]).toIfHeldDown("l⇧", ["l⌘⌥"]),
-      mapSimultaneous(["s", "d", "f"]).toIfHeldDown("l⌃", ["l⌘⌥"]),
+      mapSimultaneous([L_SHIFT, L_CTRL, L_ALT]).toIfHeldDown("left⌘", ["left⌥⌃"]),
+      mapSimultaneous([L_SHIFT, L_ALT, L_GUI]).toIfHeldDown("left⇧", ["left⌘⌃"]),
+      mapSimultaneous([L_CTRL, L_ALT, L_GUI]).toIfHeldDown("left⇧", ["left⌘⌥"]),
       //
       // Two - left hand
-      mapSimultaneous(["a", "s"], { key_down_order: "strict" })
-        .toIfAlone("a")
-        .toIfAlone("s")
-        .toIfHeldDown("l⇧", "l⌃"),
-      mapSimultaneous(["s", "a"], { key_down_order: "strict" })
-        .toIfAlone("s")
-        .toIfAlone("a")
-        .toIfHeldDown("l⇧", "l⌃"),
-      mapSimultaneous(["a", "d"], { key_down_order: "strict" })
-        .toIfAlone("a")
-        .toIfAlone("d")
-        .toIfHeldDown("l⇧", "l⌥"),
-      mapSimultaneous(["d", "a"], { key_down_order: "strict" })
-        .toIfAlone("d")
-        .toIfAlone("a")
-        .toIfHeldDown("l⇧", "l⌥"),
-      mapSimultaneous(["a", "f"], { key_down_order: "strict" })
-        .toIfAlone("a")
-        .toIfAlone("f")
-        .toIfHeldDown("l⇧", "l⌘"),
-      mapSimultaneous(["f", "a"], { key_down_order: "strict" })
-        .toIfAlone("f")
-        .toIfAlone("a")
-        .toIfHeldDown("l⇧", "l⌘"),
-      mapSimultaneous(["s", "d"], { key_down_order: "strict" })
-        .toIfAlone("s")
-        .toIfAlone("d")
-        .toIfHeldDown("l⌃", "l⌥"),
-      mapSimultaneous(["d", "s"], { key_down_order: "strict" })
-        .toIfAlone("d")
-        .toIfAlone("s")
-        .toIfHeldDown("l⌃", "l⌥"),
-      mapSimultaneous(["s", "f"], { key_down_order: "strict" })
-        .toIfAlone("s")
-        .toIfAlone("f")
-        .toIfHeldDown("l⌃", "l⌘"),
-      mapSimultaneous(["f", "s"], { key_down_order: "strict" })
-        .toIfAlone("f")
-        .toIfAlone("s")
-        .toIfHeldDown("l⌃", "l⌘"),
-      mapSimultaneous(["d", "f"], { key_down_order: "strict" })
-        .toIfAlone("d")
-        .toIfAlone("f")
-        .toIfHeldDown("l⌥", "l⌘"),
-      mapSimultaneous(["f", "d"], { key_down_order: "strict" })
-        .toIfAlone("f")
-        .toIfAlone("d")
-        .toIfHeldDown("l⌥", "l⌘"),
+      mapSimultaneous([L_SHIFT, L_CTRL], { key_down_order: "strict" })
+        .toIfAlone(L_SHIFT)
+        .toIfAlone(L_CTRL)
+        .toIfHeldDown("left⇧", "left⌃"),
+      mapSimultaneous([L_CTRL, L_SHIFT], { key_down_order: "strict" })
+        .toIfAlone(L_CTRL)
+        .toIfAlone(L_SHIFT)
+        .toIfHeldDown("left⇧", "left⌃"),
+      mapSimultaneous([L_SHIFT, L_ALT], { key_down_order: "strict" })
+        .toIfAlone(L_SHIFT)
+        .toIfAlone(L_ALT)
+        .toIfHeldDown("left⇧", "left⌥"),
+      mapSimultaneous([L_ALT, L_SHIFT], { key_down_order: "strict" })
+        .toIfAlone(L_ALT)
+        .toIfAlone(L_SHIFT)
+        .toIfHeldDown("left⇧", "left⌥"),
+      mapSimultaneous([L_SHIFT, L_GUI], { key_down_order: "strict" })
+        .toIfAlone(L_SHIFT)
+        .toIfAlone(L_GUI)
+        .toIfHeldDown("left⇧", "left⌘"),
+      mapSimultaneous([L_GUI, L_SHIFT], { key_down_order: "strict" })
+        .toIfAlone(L_GUI)
+        .toIfAlone(L_SHIFT)
+        .toIfHeldDown("left⇧", "left⌘"),
+      mapSimultaneous([L_CTRL, L_ALT], { key_down_order: "strict" })
+        .toIfAlone(L_CTRL)
+        .toIfAlone(L_ALT)
+        .toIfHeldDown("left⌃", "left⌥"),
+      mapSimultaneous([L_ALT, L_CTRL], { key_down_order: "strict" })
+        .toIfAlone(L_ALT)
+        .toIfAlone(L_CTRL)
+        .toIfHeldDown("left⌃", "left⌥"),
+      mapSimultaneous([L_CTRL, L_GUI], { key_down_order: "strict" })
+        .toIfAlone(L_CTRL)
+        .toIfAlone(L_GUI)
+        .toIfHeldDown("left⌃", "left⌘"),
+      mapSimultaneous([L_GUI, L_CTRL], { key_down_order: "strict" })
+        .toIfAlone(L_GUI)
+        .toIfAlone(L_CTRL)
+        .toIfHeldDown("left⌃", "left⌘"),
+      mapSimultaneous([L_ALT, L_GUI], { key_down_order: "strict" })
+        .toIfAlone(L_ALT)
+        .toIfAlone(L_GUI)
+        .toIfHeldDown("left⌥", "left⌘"),
+      mapSimultaneous([L_GUI, L_ALT], { key_down_order: "strict" })
+        .toIfAlone(L_GUI)
+        .toIfAlone(L_ALT)
+        .toIfHeldDown("left⌥", "left⌘"),
       //
       // One - left hand
-      map("a")
-        .toIfAlone("a", {}, { halt: true })
-        .toDelayedAction(toKey("vk_none"), toKey("a"))
-        .toIfHeldDown("l⇧", {}, { halt: true }),
-      map("s")
-        .toIfAlone("s", {}, { halt: true })
-        .toDelayedAction(toKey("vk_none"), toKey("s"))
-        .toIfHeldDown("l⌃", {}, { halt: true }),
-      map("d")
-        .toIfAlone("d", {}, { halt: true })
-        .toDelayedAction(toKey("vk_none"), toKey("d"))
-        .toIfHeldDown("l⌥", {}, { halt: true }),
-      map("f")
-        .toIfAlone("f", {}, { halt: true })
-        .toDelayedAction(toKey("vk_none"), toKey("f", {}, { halt: true }))
-        .toIfHeldDown("l⌘", {}, { halt: true }),
+      map(L_SHIFT)
+        .toIfAlone(L_SHIFT, {}, { halt: true })
+        .toDelayedAction(toKey("vk_none"), toKey(L_SHIFT))
+        .toIfHeldDown("left⇧", {}, { halt: true }),
+      map(L_CTRL)
+        .toIfAlone(L_CTRL, {}, { halt: true })
+        .toDelayedAction(toKey("vk_none"), toKey(L_CTRL))
+        .toIfHeldDown("left⌃", {}, { halt: true }),
+      map(L_ALT)
+        .toIfAlone(L_ALT, {}, { halt: true })
+        .toDelayedAction(toKey("vk_none"), toKey(L_ALT))
+        .toIfHeldDown("left⌥", {}, { halt: true }),
+      map(L_GUI)
+        .toIfAlone(L_GUI, {}, { halt: true })
+        .toDelayedAction(toKey("vk_none"), toKey(L_GUI, {}, { halt: true }))
+        .toIfHeldDown("left⌘", {}, { halt: true }),
       //
       //
       // Four - right hand
-      mapSimultaneous(["j", "k", "l", ";"]).toIfHeldDown("r⇧", ["r⌘⌥⌃"]),
+      mapSimultaneous([R_GUI, R_CTRL, R_ALT, R_SHIFT]).toIfHeldDown("right⇧", ["right⌘⌥⌃"]),
       //
       // Three - right hand
-      mapSimultaneous([";", "l", "k"]).toIfHeldDown("r⇧", ["r⌥⌃"]),
-      mapSimultaneous([";", "k", "j"]).toIfHeldDown("r⇧", ["r⌘⌥"]),
-      mapSimultaneous(["l", "k", "j"]).toIfHeldDown("r⌃", ["r⌘⌥"]),
+      mapSimultaneous([R_SHIFT, R_ALT, R_CTRL]).toIfHeldDown("right⇧", ["right⌥⌃"]),
+      mapSimultaneous([R_SHIFT, R_CTRL, R_GUI]).toIfHeldDown("right⇧", ["right⌘⌥"]),
+      mapSimultaneous([R_ALT, R_CTRL, R_GUI]).toIfHeldDown("right⌃", ["right⌘⌥"]),
       //
       // Two - right hand
-      mapSimultaneous([";", "l"], { key_down_order: "strict" })
-        .toIfAlone(";")
-        .toIfAlone("l")
-        .toIfHeldDown("r⇧", "r⌃"),
-      mapSimultaneous(["l", ";"], { key_down_order: "strict" })
-        .toIfAlone("l")
-        .toIfAlone(";")
-        .toIfHeldDown("r⇧", "r⌃"),
-      mapSimultaneous([";", "k"], { key_down_order: "strict" })
-        .toIfAlone(";")
-        .toIfAlone("k")
-        .toIfHeldDown("r⇧", "r⌥"),
-      mapSimultaneous(["k", ";"], { key_down_order: "strict" })
-        .toIfAlone("k")
-        .toIfAlone(";")
-        .toIfHeldDown("r⇧", "r⌥"),
-      mapSimultaneous([";", "j"], { key_down_order: "strict" })
-        .toIfAlone(";")
-        .toIfAlone("j")
-        .toIfHeldDown("r⇧", "r⌘"),
-      mapSimultaneous(["j", ";"], { key_down_order: "strict" })
-        .toIfAlone("j")
-        .toIfAlone(";")
-        .toIfHeldDown("r⇧", "r⌘"),
-      mapSimultaneous(["l", "k"], { key_down_order: "strict" })
-        .toIfAlone("l")
-        .toIfAlone("k")
-        .toIfHeldDown("r⌃", "r⌥"),
-      mapSimultaneous(["k", "l"], { key_down_order: "strict" })
-        .toIfAlone("k")
-        .toIfAlone("l")
-        .toIfHeldDown("r⌃", "r⌥"),
-      mapSimultaneous(["l", "j"], { key_down_order: "strict" })
-        .toIfAlone("l")
-        .toIfAlone("j")
-        .toIfHeldDown("r⌃", "r⌘"),
-      mapSimultaneous(["j", "l"], { key_down_order: "strict" })
-        .toIfAlone("j")
-        .toIfAlone("l")
-        .toIfHeldDown("r⌃", "r⌘"),
-      mapSimultaneous(["k", "j"], { key_down_order: "strict" })
-        .toIfAlone("k")
-        .toIfAlone("j")
-        .toIfHeldDown("r⌥", "r⌘"),
-      mapSimultaneous(["j", "k"], { key_down_order: "strict" })
-        .toIfAlone("j")
-        .toIfAlone("k")
-        .toIfHeldDown("r⌥", "r⌘"),
+      mapSimultaneous([R_SHIFT, R_ALT], { key_down_order: "strict" })
+        .toIfAlone(R_SHIFT)
+        .toIfAlone(R_ALT)
+        .toIfHeldDown("right⇧", "right⌃"),
+      mapSimultaneous([R_ALT, R_SHIFT], { key_down_order: "strict" })
+        .toIfAlone(R_ALT)
+        .toIfAlone(R_SHIFT)
+        .toIfHeldDown("right⇧", "right⌃"),
+      mapSimultaneous([R_SHIFT, R_CTRL], { key_down_order: "strict" })
+        .toIfAlone(R_SHIFT)
+        .toIfAlone(R_CTRL)
+        .toIfHeldDown("right⇧", "right⌥"),
+      mapSimultaneous([R_CTRL, R_SHIFT], { key_down_order: "strict" })
+        .toIfAlone(R_CTRL)
+        .toIfAlone(R_SHIFT)
+        .toIfHeldDown("right⇧", "right⌥"),
+      mapSimultaneous([R_SHIFT, R_GUI], { key_down_order: "strict" })
+        .toIfAlone(R_SHIFT)
+        .toIfAlone(R_GUI)
+        .toIfHeldDown("right⇧", "right⌘"),
+      mapSimultaneous([R_GUI, R_SHIFT], { key_down_order: "strict" })
+        .toIfAlone(R_GUI)
+        .toIfAlone(R_SHIFT)
+        .toIfHeldDown("right⇧", "right⌘"),
+      mapSimultaneous([R_ALT, R_CTRL], { key_down_order: "strict" })
+        .toIfAlone(R_ALT)
+        .toIfAlone(R_CTRL)
+        .toIfHeldDown("right⌃", "right⌥"),
+      mapSimultaneous([R_CTRL, R_ALT], { key_down_order: "strict" })
+        .toIfAlone(R_CTRL)
+        .toIfAlone(R_ALT)
+        .toIfHeldDown("right⌃", "right⌥"),
+      mapSimultaneous([R_ALT, R_GUI], { key_down_order: "strict" })
+        .toIfAlone(R_ALT)
+        .toIfAlone(R_GUI)
+        .toIfHeldDown("right⌃", "right⌘"),
+      mapSimultaneous([R_GUI, R_ALT], { key_down_order: "strict" })
+        .toIfAlone(R_GUI)
+        .toIfAlone(R_ALT)
+        .toIfHeldDown("right⌃", "right⌘"),
+      mapSimultaneous([R_CTRL, R_GUI], { key_down_order: "strict" })
+        .toIfAlone(R_CTRL)
+        .toIfAlone(R_GUI)
+        .toIfHeldDown("right⌥", "right⌘"),
+      mapSimultaneous([R_GUI, R_CTRL], { key_down_order: "strict" })
+        .toIfAlone(R_GUI)
+        .toIfAlone(R_CTRL)
+        .toIfHeldDown("right⌥", "right⌘"),
       //
       // One - right hand
-      map(";")
-        .toIfAlone(";", {}, { halt: true })
-        .toDelayedAction(toKey("vk_none"), toKey(";"))
-        .toIfHeldDown("r⇧", {}, { halt: true }),
-      map("l")
-        .toIfAlone("l", {}, { halt: true })
-        .toDelayedAction(toKey("vk_none"), toKey("l"))
-        .toIfHeldDown("r⌃", {}, { halt: true }),
-      map("k")
-        .toIfAlone("k", {}, { halt: true })
-        .toDelayedAction(toKey("vk_none"), toKey("k"))
-        .toIfHeldDown("r⌥", {}, { halt: true }),
-      map("j")
-        .toIfAlone("j", {}, { halt: true })
-        .toDelayedAction(toKey("vk_none"), toKey("j"))
-        .toIfHeldDown("r⌘", {}, { halt: true }),
+      map(R_SHIFT)
+        .toIfAlone(R_SHIFT, {}, { halt: true })
+        .toDelayedAction(toKey("vk_none"), toKey(R_SHIFT))
+        .toIfHeldDown("right⇧", {}, { halt: true }),
+      map(R_ALT)
+        .toIfAlone(R_ALT, {}, { halt: true })
+        .toDelayedAction(toKey("vk_none"), toKey(R_ALT))
+        .toIfHeldDown("right⌃", {}, { halt: true }),
+      map(R_CTRL)
+        .toIfAlone(R_CTRL, {}, { halt: true })
+        .toDelayedAction(toKey("vk_none"), toKey(R_CTRL))
+        .toIfHeldDown("right⌥", {}, { halt: true }),
+      map(R_GUI)
+        .toIfAlone(R_GUI, {}, { halt: true })
+        .toDelayedAction(toKey("vk_none"), toKey(R_GUI))
+        .toIfHeldDown("right⌘", {}, { halt: true }),
     ]),
   ],
   {
